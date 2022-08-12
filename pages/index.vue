dc2d805e08472aa2a212301fed1b43896e202863
Jaden Chant <chant1@stolaf.edu>
2022-08-11 22:48:57 -0500

    added about page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <Tutorial />
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
 </template>
 
 <script lang="ts">
